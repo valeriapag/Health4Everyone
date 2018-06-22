7df2dd80a304d377cad52767afb3a362c5d8b60e
Jan <Jgutnik3@gmail.com>
2018-06-22 22:11:07 +0200

    Praktikumsaufgabe 2c

--- public/SERVERTEST3.js
+++ public/SERVERTEST3.js
@@ -8,25 +8,45 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 //Modul crypto wird eingebunden
 var crypto = require('crypto');
-//Modul Bodyparser
+//Modul Bodyparser wird eingebunden
 var bodyparser = require('body-parser');
+//Modul Express-session wird eingebunden
+var session = require('express-session');
+//Modul uuid wird eingebunden
+var uuid = require('uuid');
 
 //Benutze Bodyparser zum Zugriff auf gepostete Daten
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
 /*
+    Session wird konfiguriert, mit genid wird eine ID für die jeweilige Session erstellt
+ */
+app.use(session({
+    genid: function(req) {
+        return uuid.v1() // use UUIDs for session IDs
+    },
+    secret: 'blue sky',
+    name : 'sessID',
+    resave: false,
+    saveUninitialized: false
+}))
+
+/*
     Callback Funktion fuer Post Anfragen (login_page) des Clients.
-    Je nach eingegebenem Username und Passwort wird zu "/informatiker_page" "/patient_page" oder "arzt_page"
+    Je nach eingegebenem Username und Passwort wird zu "/meti_page" "/patient_page" oder "arzt_page"
     weitergeleitet.
-    Überprüfen des Passworts durch Umwandlung in md5 Hashwert und Vergleichen mit Hashwerten in der Datenbank.
+    Überprüfen des Passworts durch Umwandlung in md5 Hashwert und Vergleichen mit Pw Hashwerten in der Datenbank.
  */
 app.post("/login_page", function(req, res) {
     //Pruefe ob alle Felder eingegeben
     //console.log(req.body.uname);
     //console.log(req.body.psw);
-    var logged_in = 0;
     if (req.body.uname && req.body.psw) {
+        if (req.session.UID) {
+            console.log("Bereits eingeloggt!");
+            res.redirect("")
+        }
         MongoClient.connect('mongodb://127.0.0.1:27017/Ourdb', {useNewUrlParser: true}, async function (error, db) {
             //Fehler wird abgefangen
             try {
@@ -38,17 +58,32 @@ app.post("/login_page", function(req, res) {
                 var meti_count = await ourdb.collection('meti').find({'pwHash': checkHASH, 'uname': String(req.body.uname)}).count();
                 var patient_count = await ourdb.collection('patient').find({'pwHash': checkHASH, 'uname': String(req.body.uname)}).count();
                 var arzt_count = await ourdb.collection('arzt').find({'pwHash': checkHASH, 'uname': String(req.body.uname)}).count();
+                var meti_ID = await ourdb.collection('meti').find({'pwHash': checkHASH, 'uname': String(req.body.uname)}).toArray();
+                meti_ID = meti_ID[0].metiID;
+                var patient_ID = await ourdb.collection('meti').find({'pwHash': checkHASH, 'uname': String(req.body.uname)}).toArray();
+                patient_ID = patient_ID[0].patientID;
+                var arzt_ID = await ourdb.collection('meti').find({'pwHash': checkHASH, 'uname': String(req.body.uname)}).toArray();
+                arzt_ID = arzt_ID[0].arztID;
                 //console.log(await ourdb.collection('meti').find({'pwHASH': checkHASH, 'uname': req.body.uname}));
                 if (await meti_count >= 1) {
-                    await console.log('FUNZT meti');
-                    res.redirect('/informatiker_page');
+                    req.session.status = 'meti';
+                    req.session.UID = meti_ID;
+                    await console.log("FUNZT meti");
+                    await console.log(req.session);
+                    res.redirect('/meti_page');
                 }
                 else if (await patient_count >= 1) {
+                    req.session.status = 'patient';
+                    req.session.UID = patient_ID;
                     await console.log('FUNZT patient');
+                    await console.log(req.session);
                     res.redirect('/patient_page');
                 }
                 else if (await arzt_count >= 1) {
-                    await console.log('FUNZT arzt');
+                    req.session.status = 'arzt';
+                    req.session.UID = arzt_ID;
+                    await console.log('FUNZT patient');
+                    await console.log(req.session);
                     res.redirect('/arzt_page');
                 }
                 else {
@@ -63,28 +98,54 @@ app.post("/login_page", function(req, res) {
     }
 });
 
+/*
+app.all("/secure/*", function(req, res, next) {
+
+});
+*/
+
 app.get("/", function(req, res) {
+    if (req.session.views) {
+        req.session.views++
+    } else {
+        req.session.views = 1
+    }
     res.sendFile(__dirname + '/index.html');
     console.log('Verbunden');
 });
 
 app.get("/login_page", function(req, res) {
-    res.sendFile(__dirname + '/login_page.html');
+    res.sendFile(__dirname + '/secure/login_page.html');
     console.log('Verbunden');
 });
 
-app.get("/informatiker_page", function(req, res) {
-    res.sendFile(__dirname + '/informatiker_page.html');
+app.get("/meti_page", function(req, res) {
+    res.sendFile(__dirname + '/secure/meti_page.html');
     console.log('Verbunden');
 });
 
 app.get("/patient_page", function(req, res) {
-    res.sendFile(__dirname + '/patient_page.html');
+    res.sendFile(__dirname + '/secure/patient_page.html');
     console.log('Verbunden');
 });
 
 app.get("/arzt_page", function(req, res) {
-    res.sendFile(__dirname + '/arzt_page.html');
+    res.sendFile(__dirname + '/secure/arzt_page.html');
+    console.log('Verbunden');
+});
+
+app.get("/ausloggen", function(req, res) {
+    res.sendFile(__dirname + '/secure/ausloggen.html');
+    console.log('Verbunden');
+});
+
+app.get("/beschwerde", function(req, res) {
+    res.sendFile(__dirname + '/secure/beschwerde.html');
+    console.log('Verbunden');
+});
+
+app.get("/konto_aktivieren", function(req, res) {
+    res.sendFile(__dirname + '/secure/arzt_page.html');
     console.log('Verbunden');
 });
 
