ff2bc4a3cfe48dd39e81fb312133cec554574700
Jan <Jgutnik3@gmail.com>
2018-06-23 23:21:26 +0200

    Praktikumsaufgabe 2c

--- public/SERVERTEST2.js
+++ public/SERVERTEST2.js
@@ -2,10 +2,13 @@
 var MongoClient = require('mongodb').MongoClient;
 //Modul crypto wird eingebunden
 var crypto = require('crypto');
+//Modul uniqid wird eingebunden
+var uniqid = require('uniqid');
 
 //Verbinden mit mongodb (Mongodb Server muss laufen!) mittels MongoClient.connect() Callback
 //Name der Db = Ourdb
-MongoClient.connect('mongodb://127.0.0.1:27017/Ourdb', {useNewUrlParser: true}, function(error, db) {
+MongoClient.connect('mongodb://127.0.0.1:27017/Ourdb', function(error, db) {
+
     //Fehler wird abgefangen
     if (error) {
         throw error;
@@ -15,56 +18,90 @@ MongoClient.connect('mongodb://127.0.0.1:27017/Ourdb', {useNewUrlParser: true}, 
     //Erstelle Variable zum Zugriff auf Db für spätere Operationen
     var ourdb = db.db("Ourdb");
     //Erstelle Collection "patienten", "arzt" und "meti"
-    ourdb.createCollection("patient", function(err, db) {
+    ourdb.createCollection("patient", function(err, collection) {
+
         console.log("Collection patient erstellt");
 
-        ourdb.createCollection("arzt", function(err, db){
+        ourdb.createCollection("arzt", function (err, collection) {
             console.log("Collection arzt erstellt");
 
-            ourdb.createCollection("meti", function(err, db) {
+            ourdb.createCollection("meti", function (err, collection) {
                 console.log("Collection meti erstellt");
 
+                ourdb.createCollection("beschwerde", function (err, collection) {
+                    console.log('Collection beschwerde erstellt');
 
-                var pw = '1234';
-                var HASH = String(crypto.createHash('md5').update(pw).digest('hex'));
-                var metiNew = {
-                    'vname' : 'Gustav',
-                    'nname' : 'Peterson',
-                    'alter' : 25,
-                    'geschlecht' : 'm',
-                    'uname' : 'gPeterson',
-                    'pwHash' : HASH,
-                };
-                ourdb.collection('meti').insertOne(metiNew, function (err, db) {
-                    db.close();
-                    console.log("The end");
+                    ourdb.createCollection("Aktivierung", function (err, collection) {
+                        console.log('Collection Aktivierung erstellt');
+
+                        var pw = '1234', HASH = crypto.createHash('md5').update(pw).digest('hex'), meti_id = uniqid(),
+                            metiNew = {
+                                'metiID': meti_id,
+                                'vname': 'Gustav',
+                                'nname': 'Peterson',
+                                'alter': 25,
+                                'geschlecht': 'm',
+                                'email' : null,
+                                'uname': 'gPeterson',
+                                'pwHash': HASH
+                            };
+                        ourdb.collection('meti').insertOne(metiNew, function (err, result) {
+
+                            var patient_id = uniqid(), pw = '2345',
+                                HASH = crypto.createHash('md5').update(pw).digest('hex'),
+                                patientNew = {
+                                    'patientID': patient_id,
+                                    'vname': 'Max',
+                                    'nname': 'Brot',
+                                    'alter': 30,
+                                    'geschlecht': 'm',
+                                    'email' : null,
+                                    'uname': 'mBrot',
+                                    'pwHash': HASH,
+                                    'krankheit': null,
+                                    'EEG': null,
+                                    'EKG': null,
+                                    'GGT': null,
+                                    'LVolumen': null,
+                                    'arztID': null,
+                                    'beschwerdeID' : null
+                                };
+                            ourdb.collection('patient').insertOne(patientNew, function (err, result) {
+
+                                var arzt_id = uniqid(), pw = '3456',
+                                    HASH = crypto.createHash('md5').update(pw).digest('hex'),
+                                    arztNew = {
+                                        'arztID': arzt_id,
+                                        'vname': 'Boris',
+                                        'nname': 'Bwah',
+                                        'alter': 30,
+                                        'geschlecht': 'm',
+                                        'email' : null,
+                                        'fachbereich': 'Neurologie',
+                                        'uname': 'bBwah',
+                                        'pwHash': HASH
+                                    };
+                                ourdb.collection('arzt').insertOne(arztNew, function (err, result) {
+
+                                    var patient_id = uniqid(), aktivPW = uniqid(),
+                                        aktNew = {
+                                            'patientID' : patient_id,
+                                            'arztID' : null,
+                                            'aktPW' : aktivPW
+                                        };
+                                    console.log(aktivPW);
+
+                                    ourdb.collection('aktivierung').insertOne(aktNew, function (err, result) {
+
+                                        db.close();
+                                        console.log('The end');
+                                    });
+                                });
+                            });
+                        });
+                    });
                 });
             });
-
-
         });
-
-
-
     });
-
-
-
-    /*
-    var patientNeu = {
-        'vname' : 'Gustav',
-        'nname' : 'Peterson',
-        'alter' : 82,
-        'geschlecht' : 'm',
-        'uname' : 'gPeterson',
-        'pwHash' : HASH,
-        'krankheit' : null,
-        'EEG' : null,
-        'EKG' : null,
-        'GGT' : null,
-        'LVolumen' : null,
-        'arztID' : null
-    };
-    */
-
 });
